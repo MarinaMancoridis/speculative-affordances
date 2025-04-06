93a3800c4b9aea63480faf71826002a5ca4fc996
MarinaMancoridis <marina.mancoridis@gmail.com>
2025-04-06 10:40:09 -0400

    fixed asynch bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,28 @@
     import mapboxgl from "mapbox-gl";
     import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
     import { onMount } from "svelte";
+    import localData from "./../data/mapping_inequality_redlining.json";
 
     mapboxgl.accessToken = "pk.eyJ1IjoibWFyaW5hLW1hbmNvcmlkaXMiLCJhIjoiY205NXBjZmx3MWNkZjJzcHc0dDVlYXFodCJ9.mS5MAGr-YmpGput97-3htA";
 
-    onMount(() => {
-		const map = new mapboxgl.Map({
-			container: 'map', // HTML element ID
-			style: 'mapbox://styles/marina-mancoridis/cm95pzaws009901qt26z24os9', // Marina's grey map style
-			center: [-71.1056, 42.3736], // Cambridge/Boston (longitude, latitude)
-			zoom: 12,
-			minZoom: 10,
-			maxZoom: 18
-		});
-	});
+    onMount(async () => {
+        const map = new mapboxgl.Map({
+            container: 'map', // HTML element ID
+            style: 'mapbox://styles/marina-mancoridis/cm95pzaws009901qt26z24os9', // Marina's grey map style
+            center: [-71.1056, 42.3736], // Cambridge/Boston (longitude, latitude)
+            zoom: 12,
+            minZoom: 10,
+            maxZoom: 18
+        });
 
-    await new Promise(resolve => map.on("load", resolve));
-    map.addSource("boston_route", {
-			type: "geojson",
-			data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"
+        await new Promise(resolve => map.on("load", resolve));
+        
+        map.addSource("redlining_data", {
+            type: "geojson",
+            data: localData
+        });
     });
+    
     
 </script>
 
