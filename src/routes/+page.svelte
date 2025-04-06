b48d29b1a95df2cd1f574dd066082ca9ca5d1c9f
MarinaMancoridis <marina.mancoridis@gmail.com>
2025-04-06 09:45:21 -0400

    new front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Speculative Affordances: FP2</h1>
+<p>Lena Armstrong, Marina Mancoridis, Eagon Meng, Jon Rosario</p>
