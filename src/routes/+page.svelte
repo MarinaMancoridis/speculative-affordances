658774cf53942930f75447acc5ba85c5d479f171
Lena Armstrong <larmstrong@g.harvard.edu>
2025-04-08 01:09:20 -0400

    added red lining colors, iBought homes from Mass Records, and tool tips

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,38 @@
 
 <script>
     import mapboxgl from "mapbox-gl";
+    import * as d3 from "d3";
     import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
     import { onMount } from "svelte";
     import localData from "./../data/mapping_inequality_redlining.json";
 
-    mapboxgl.accessToken = "pk.eyJ1IjoibWFyaW5hLW1hbmNvcmlkaXMiLCJhIjoiY205NXBjZmx3MWNkZjJzcHc0dDVlYXFodCJ9.mS5MAGr-YmpGput97-3htA";
+    mapboxgl.accessToken = "pk.eyJ1IjoibGVuYWFybXMiLCJhIjoiY205N3V0aGw2MGJnMDJsb2tiNDB1czFzaiJ9.bTzeSaHAUueU3tJHFNXyAA";
+    
+    let homes = [];
+    let map = null;
+    let mapViewChanged = 0;
+
+    function getHomes (home) {
+        let point = new mapboxgl.LngLat(+home.Longitude, +home.Latitude);
+        let {x, y} = map.project(point);
+        return {cx: x, cy: y};
+    }
+
+    $: map?.on("move", evt => mapViewChanged++);
 
     onMount(async () => {
-        const map = new mapboxgl.Map({
+        
+        homes = await d3.csv("/data/mass_records.csv", row => ({
+            ...row,
+            Latitude: Number(row.Latitude), 
+            Longitude: Number(row.Longitude),
+            Address: String(row.Address),
+            Name: String(row.Name)
+        }));
+
+        map = new mapboxgl.Map({
             container: 'map', // HTML element ID
-            style: 'mapbox://styles/marina-mancoridis/cm95pzaws009901qt26z24os9', // Marina's grey map style
+            style: 'mapbox://styles/mapbox/streets-v12',
             center: [-71.1056, 42.3736], // Cambridge/Boston (longitude, latitude)
             zoom: 11.5,
             minZoom: 10,
@@ -28,20 +50,35 @@
         });
 
         map.addLayer({
-            id: "redlining_layer",
-            type: "line",
+            id: "grade_color_layer",
+            type: "fill",
             source: "redlining_data",
             paint: {
-                "line-color": "#ad494e",
-                "line-opacity": 0.5,
-                "line-width": 2
+                "fill-color": ["get", "fill"],
+                "fill-opacity": 0.3,
+                "fill-outline-color": "#ad494e"
             },
         });
+
+        console.log(homes)
+
     });
-    
-    
+
 </script>
 
 <h1>Speculative Affordances: FP2</h1>
 <p>Lena Armstrong, Marina Mancoridis, Eagon Meng, Jon Rosario</p>
 <br>
+
+<div id="map">
+	<svg>
+        {#key mapViewChanged}
+            {#each homes as home}
+                <circle { ...getHomes(home) } r="5" fill="steelblue">
+                    <title>Purchased by: {home.Name}</title>
+                </circle> 
+                
+            {/each}
+        {/key}
+	</svg>
+</div>
