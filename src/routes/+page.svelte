4632d9938deeb990e7888ffa681aaa487c9667d4
Jon Rosario <jonros@mit.edug>
2025-04-28 11:26:43 -0400

    Empty commit to force redeploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1791,7 +1791,7 @@
                 </div>
                 <div id="chart-wrapper" style="max-width:900px; margin:3em auto 0; text-align:center;">
                     <div id="chart-tooltip" class="dialogue-box"></div>
-                    <h2 class="chart-title">Average Zestimate Trends by HOLC District</h2>
+                    <h2 class="chart-title">Average Zestimate Trends by HOLC District.</h2>
                     <div id="zestimate-chart" style="width:100%; height:500px;"></div>
                     <div id="legend"></div>
                 </div>
