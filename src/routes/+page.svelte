85c6392b995b6d46d8b5ebad53b64ca7c429d272
MarinaMancoridis <marina.mancoridis@gmail.com>
2025-05-05 16:48:23 -0400

    marina modularized

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,8 @@
     import NotIBoughtHomes from "./NotIBoughtHomes.svelte";
     import EverythingIsGettingExpensive from "./EverythingIsGettingExpensive.svelte";
     import Intro from "./intro.svelte";
-    import Somerville from "./somerville.svelte"
+    import Somerville from "./somerville.svelte";
+    import StickyChart from "./corpownership.svelte";
 
     // scroll states
     let scrollProgress = 0;
@@ -580,21 +581,8 @@
         <!-- VIZ only shows once you've scrolled through all previous steps -->
         <svelte:fragment slot="viz">
 
-            <div class="after-todo">  
-                
-            <div id="sticky-container">
-                <div class="chart-text">
-                    <p> Corporate ownership quietly reshapes who gets to call Boston home—not by moving into neighborhoods, but <b>by betting on their future</b>. 
-                    </p><p>These firms don't buy homes to live in them; <b>they speculate</b>. iBuying is <i>a form of corporate speculation</i>, wherein companies use these algorithms to make direct housing offers. </p>
-                    <p> <b>Scroll down</b> to explore the visualization and watch the history of how corporate speculation quietly but unmistakably alter Boston’s housing landscape over time. </p>
-                </div>
-                <div id="chart-wrapper" style="max-width:900px; margin:3em auto 0; text-align:center;">
-                    <div id="chart-tooltip" class="dialogue-box"></div>
-                    <h2 class="chart-title">Corporate Ownership Rate Over Time</h2>
-                    <div id="corp-own-chart" style="width:100%; height:500px;"></div>
-                </div>
-            </div>
-
+        <div class="after-todo">
+            <StickyChart/>
 
             <div class="post-chart">
                 <br><br><br><br>
