0649538eea014d8e65516353f32eeff111b813da
MarinaMancoridis <marina.mancoridis@gmail.com>
2025-05-07 14:06:32 -0400

    redid summary section

--- src/routes/final.svelte
+++ src/routes/final.svelte
@@ -4,18 +4,18 @@
 <div id="takeaways">
     <h3>Summary of Findings</h3>
     <ul>
-    <li>iBuying is prevalent in Boston, especially for more afforadble housing.</li>
-    <li>Redlining and discriminatory housing practices in the past continue to affect present housing and consquently AI real estate pricing models.</li>
-    <li>iBought homes are not bought for fair prices.</li>
+    <li><b>iBuying is active in the Greater Boston Area</b>, clustering in historically redlined neighborhoods—raising concerns that algorithms might inadvertently reinforce existing inequalities.</li>
+    <li><b>The convenience of iBuying comes at a cost</b>. Most homes sell below market-value estimates (<i>Zestimates</i>), subtly transferring wealth from homeowners to corporate speculators.</li>
+    <li><b>Transparency is lacking</b>. The full scope of iBuying remains hidden, making its true impact likely larger than current data suggest.</li>
     </ul>
 </div>
 
 <div id="action">
     <h3>What can we do?</h3>
     <ul>
-    <li> Reconsider selling your home to iBuyers. If you do, make sure you are getting a fair price by checking its value through Zillow's <a id="action-links" href="https://www.zillow.com/how-much-is-my-home-worth/">Zestimate tool</a>.</li>
-    <li> Reach out to <a id="action-links" href="https://www.ftc.gov/news-events/news/press-releases/2024/04/ftc-sends-nearly-62-million-refunds-sellers-deceived-online-real-estate-listing-service-opendoor">Federal Trade Commission</a> if you suspect that you have been given an unfair price by an iBuyer.</li>
-    <li> Advocate for protections against automated speculation practices.</li>
+    <li> <b>Reconsider selling your home to iBuyers</b>. If you do, make sure you are getting a fair price by checking its value through Zillow's <a id="action-links" href="https://www.zillow.com/how-much-is-my-home-worth/">Zestimate tool</a>.</li>
+    <li> <b>Reach out</b> to <a id="action-links" href="https://www.ftc.gov/news-events/news/press-releases/2024/04/ftc-sends-nearly-62-million-refunds-sellers-deceived-online-real-estate-listing-service-opendoor">Federal Trade Commission</a> if you suspect that you have been given an unfair price by an iBuyer.</li>
+    <li> <b>Advocate</b> for protections against automated speculation practices.</li>
     </ul>
 </div>
 
