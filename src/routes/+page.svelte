e2b4e2cdbeba9c3e4859e51c9f0c2ed9fbcf6ba2
MarinaMancoridis <marina.mancoridis@gmail.com>
2025-05-07 11:46:19 -0400

    modularized final

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,6 +37,7 @@
     import StickyChart from "./corpownership.svelte";
     import DataCollection from "./datacollection.svelte";
     import FtcRefund from "./FtcRefund.svelte";
+    import FinalTakeaways from "./final.svelte";
 
     // Zestimate helpers
     import {
@@ -679,44 +680,8 @@
                 </p>
             </div>
             <FtcRefund />
+            <FinalTakeaways />
               
-            <br><br>
-            <h1 style="text-align: center;">Final Takeaways</h1>
-            <div class="info-grid">
-                <div id="takeaways">
-                    <h3>Summary of Findings</h3>
-                    <ul>
-                    <li>iBuying is prevalent in Boston, especially for more afforadble housing.</li>
-                    <li>Redlining and discriminatory housing practices in the past continue to affect present housing and consquently AI real estate pricing models.</li>
-                    <li>iBought homes are not bought for fair prices.</li>
-                    </ul>
-                </div>
-
-                <div id="action">
-                    <h3>What can we do?</h3>
-                    <ul>
-                    <li> Reconsider selling your home to iBuyers. If you do, make sure you are getting a fair price by checking its value through Zillow's <a id="action-links" href="https://www.zillow.com/how-much-is-my-home-worth/">Zestimate tool</a>.</li>
-                    <li> Reach out to <a id="action-links" href="https://www.ftc.gov/news-events/news/press-releases/2024/04/ftc-sends-nearly-62-million-refunds-sellers-deceived-online-real-estate-listing-service-opendoor">Federal Trade Commission</a> if you suspect that you have been given an unfair price by an iBuyer.</li>
-                    <li> Advocate for protections against automated speculation practices.</li>
-                    </ul>
-                </div>
-
-                <div id="acks">
-                    <h3>Acknowledgements</h3>
-                    <p>This project was developed with guidance and feedback from the <a id="action-links" href="https://www.mapc.org/">Metropolitan Area Planning Commission (MAPC)</a>.</p>
-                </div>
-
-                <div id="references">
-                    <h3>References</h3>
-                    <ul>
-                    <li> [1] <a class="ref" href="https://www.gao.gov/assets/gao-24-106643.pdf">https://www.gao.gov/assets/gao-24-106643.pdf</a></li>
-                    <li> [2] <a class="ref" href="https://www.onlinemarketplaces.com/articles/timeline-of-zillow-ibuying/">https://www.onlinemarketplaces.com/articles/timeline-of-zillow-ibuying/</a></li>
-                    <li> [3] <a class="ref" href="https://www.ftc.gov/news-events/news/press-releases/2024/04/ftc-sends-nearly-62-million-refunds-sellers-deceived-online-real-estate-listing-service-opendoor">https://www.ftc.gov/news-events/news/press-releases/2024/04/ftc-sends-nearly-62-million-refunds-sellers-deceived-online-real-estate-listing-service-opendoor</a> </li>
-                    <li> [4] <a class="ref" href="https://investor.opendoor.com/ir-resources/faqs">https://investor.opendoor.com/ir-resources/faqs</a></li>
-                    <li> [5] <a class="ref" href="https://dl.acm.org/doi/pdf/10.1145/3630106.3659027">https://dl.acm.org/doi/pdf/10.1145/3630106.3659027</a></li>
-                    </ul>
-                </div>
-            </div>
 
         </div>
         </svelte:fragment>
