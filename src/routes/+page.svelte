2a0b2701038be293391f80f6cc0e521b43ed748e
MarinaMancoridis <marina.mancoridis@gmail.com>
2025-05-07 12:00:02 -0400

    modularized title page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,6 +40,7 @@
     import FinalTakeaways from "./final.svelte";
     import RedliningMap from "./redlining.svelte";
     import FairPrices from "./fairprices.svelte";
+    import TitlePage from "./titlepage.svelte";
 
     // Zestimate helpers
     import {
@@ -421,41 +422,15 @@
     });
 </script>
 
-<div class="grid-bg">
-    {#each Array(cols*rows) as _, i}
-      <div
-        class="grid-cell"
-        style="background-image:
-                 url({ i < replaceCount
-                          ? modifiedImgs[i]
-                          : originalImgs[i]
-                      });
-               ">
-      </div>
-    {/each}
-</div>
-
-<div class="title-section">
-    <div class="rotator">
-      <img src={$rotatingImage} alt="Rotating title image" />
-    </div>
-  
-    <div class="title-text">
-      <h1>AUTOMATION AND SPECULATION</h1>
-      <p class="typewriter-line" id="typewriter"></p>
-      <p>
-        <b>Speculative Affordances, Final Project:</b>
-        <br>
-        <i>Lena Armstrong, Marina Mancoridis, Eagon Meng, Jon Rosario</i>
-      </p>
-    </div>
-  
-    <!-- now sibling of .title-text -->
-    <button class="scroll-indicator" on:click={scrollToExplore} type="button">
-      <span>Explore</span>
-      <div class="arrow" style="margin-left:0.8rem;">↓</div>
-    </button>
-</div>
+<TitlePage 
+    {cols} 
+    {rows} 
+    {replaceCount} 
+    {modifiedImgs} 
+    {originalImgs} 
+    {rotatingImage} 
+    {scrollToExplore}
+/>
 
 
 
